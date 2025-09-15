d5c15fc48b25833ff59a24f3964ab85de1057c50
sj8687 <shreyashjadhav59807@gmail.com>
2025-09-16 00:21:14 +0530

    add docker-compose

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { PrismaClient } from "../generated/prisma/client.js";
+const app = express();
+const prisma = new PrismaClient();
+app.use(express.json());
+// Register new user
+app.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.create({
+            data: { email, password },
+        });
+        res.status(201).json(user);
+    }
+    catch (error) {
+        res.status(400).json({ error: 'User already exists or invalid data' });
+    }
+});
+// Get all users
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany({
+        select: { id: true, email: true, password: true },
+    });
+    res.json(users);
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
